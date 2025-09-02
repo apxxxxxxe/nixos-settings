820362ea2f06f363626c11d91fda1585bb0528bc
apxxxxxxe <calacium629@gmail.com>
2025-09-02 15:06:23 +0900

    a

--- flake.nix
+++ flake.nix
@@ -4,17 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     neovim-flake.url = "path:./flakes/neovim";
-    # 他の専用flakeも追加可能
   };
   
   outputs = { self, nixpkgs, neovim-flake, ... }: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         {
           nixpkgs.overlays = [
-            neovim-flake.overlay
+            neovim-flake.outputs.overlay
           ];
         }
       ];
